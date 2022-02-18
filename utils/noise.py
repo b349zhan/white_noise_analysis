ee91e52efd81b6aa6776681442199046370e3d43
Bowen Zhang <b349zhan@Bowens-MacBook-Air.local>
2022-02-18 00:21:29 -0500

    poor results, but working

--- utils/noise.py
+++ utils/noise.py
@@ -2,6 +2,10 @@ from tqdm import tqdm
 import sys
 import torch
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
 
 def generate_white_noise(model, iteration:int, batch_size:int, image_size:int, cuda:bool):
     '''
@@ -36,7 +40,10 @@ def generate_white_noise(model, iteration:int, batch_size:int, image_size:int, c
             noise[class_num].append(z[prediction==class_num].cpu())
             noisy_image_summary[class_num] += (prediction == class_num).sum()
     for class_num in range(10):
-        noise[class_num] = torch.cat(noise[class_num])
+        if noisy_image_summary[class_num]==0:
+            noise[class_num] = torch.zeros(image_size, image_size)
+        else:
+            noise[class_num] = torch.cat(noise[class_num])
     return noise, noisy_image_summary
 
 def visualize_noise_summary(noisy_image_summary):
@@ -69,7 +76,7 @@ def visualize_noise(noise,embedded=False):
             a = noise[class_label]
         plt.subplot(2, 5, class_label+1)
         plt.axis('off')
-        plt.title(str(class_label))
+        plt.title("Class:"+str(class_label))
         plt.imshow(a)
     plt.suptitle("Predictions for Randomly Generated Noises")
     plt.ioff()
@@ -90,15 +97,75 @@ def generate_embedded_noise(model, dataset, gamma:float, iteration:int, batch_si
             
         predictions = model(stimulus[:,None,...]).data.max(1)[1]
         for class_label in range(10):
+            
             class_index = (predictions == class_label)
+            if class_index.sum()==0:continue
             class_stimulus_mean = stimulus[class_index].mean(dim=0)
             class_noise_mean = noise[class_index].mean(dim=0)
             class_stimulus[class_label].append(class_stimulus_mean)
             class_noise[class_label].append(class_noise_mean)
             
     for class_label in range(10):
+        if len(class_noise[class_label])==0:
+            class_noise[class_label] = torch.zeros(image_size, image_size)
+            continue
         class_noise[class_label] = torch.mean(torch.stack(class_noise[class_label]),dim=0)
     return class_noise
 
+def feed_back_visualize(model, dataset, noise_map):
+    predictions = []
+    for class_label in range(10):
+        batch_noise_image = noise_map[class_label].reshape(1,1,noise_map[0].shape[0],-1)
+        batch_noise_image = batch_noise_image - batch_noise_image.min().item()
+        batch_noise_image_normalized = batch_noise_image / batch_noise_image.max().item()
+        output = model(batch_noise_image_normalized)
+        prediction = torch.argmax(output)
+        predictions.append(prediction.data)
+    confusion = confusion_matrix(list(range(10)) ,predictions)
+    plt.figure(figsize = (8,8))
+    confusion = confusion/np.sum(confusion, axis = 1)
+    confusionDf = pd.DataFrame(confusion, index=[i for i in list(range(10))], columns=[
+                             i for i in list(range(10))])
+    sns.heatmap(round(confusionDf, 2), annot=True, annot_kws={"size": 14})
+    plt.title("Confusion Matrix for Noise Maps", fontsize=10)
+    
+def get_normalized_noise_map(noise_map):
+    noise_map = noise_map.numpy()
+    normalized_noise_map = (noise_map - noise_map.min()) / (noise_map.max() - noise_map.min())
+    return torch.from_numpy(normalized_noise_map)
 
-                
+def noise_map_classify_and_report(data_loader, noise_maps):
+    noise_maps_normalized = [ noise_maps[class_label] for class_label in noise_maps]
+    noise_maps_classifier = torch.stack(noise_maps_normalized,dim=0)
+    truth = data_loader.dataset.targets
+    predictions = []
+    for (data, label) in data_loader:
+        data = data.reshape(data.shape[0],1, data.shape[-1], data.shape[-1])
+        probabilities = torch.sum(data * noise_maps_classifier, dim = [-2,-1])
+        batch_prediction = torch.argmax(probabilities, dim = -1)
+        predictions.extend(batch_prediction)
+    confusion = confusion_matrix(truth, predictions, normalize = "true")
+    plt.figure(figsize = (8,8))
+    confusionDf = pd.DataFrame(confusion, index=[i for i in list(range(10))], columns=[
+                             i for i in list(range(10))])
+    sns.heatmap(round(confusionDf, 2), annot=True, annot_kws={"size": 14})
+    plt.title("Confusion Matrix for Noise Maps Classifier Test Dataset Performance", fontsize=10)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
